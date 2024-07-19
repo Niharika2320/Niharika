ffe4e3ca3f434d2574d14d663d24f369effcb46f
username <wecp-dummy@gmail.com>
2024-07-19 06:11:42 +0000

    wecp initial commit

---
+++ backend/src/main/java/com/wecp/progressive/entity/CreditCard.java
@@ -0,0 +1,16 @@
+package com.wecp.progressive.entity;
+
+public class CreditCard {
+
+    private int id;
+    private String cardNumber;
+    private String cardHolderName;
+    public CreditCard() {
+    }
+    public CreditCard(int id, String cardNumber, String cardHolderName) {
+        this.id = id;
+        this.cardNumber = cardNumber;
+        this.cardHolderName = cardHolderName;
+    }
+//
+}
